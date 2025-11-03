0e15ed2a9e7134b97dcd4898cdd0ebd7ba20e1e4
Alex Hort-Francis <ajhf@hotmail.co.uk>
2025-11-03 19:54:18 +0000

    feat: Set up project - Make basic one-page site

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import cloudflareLogo from './assets/Cloudflare_Logo.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState('unknown')
 
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-        <a href='https://workers.cloudflare.com/' target='_blank'>
-          <img src={cloudflareLogo} className='logo cloudflare' alt='Cloudflare logo' />
-        </a>
-      </div>
-      <h1>Vite + React + Cloudflare</h1>
-      <div className='card'>
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          aria-label='increment'
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className='card'>
-        <button
-          onClick={() => {
-            fetch('/api/')
-              .then((res) => res.json() as Promise<{ name: string }>)
-              .then((data) => setName(data.name))
-          }}
-          aria-label='get name'
-        >
-          Name from API is: {name}
-        </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <header>
+        <h1>Roots</h1>
+      </header>
+      <main>
+        <div className='callout'>
+          <p>We use{' '}
+            <span className='new-tech'>new technologies</span>{' '}<br />
+            to bring{' '}
+            <span className='communities-together'>communities together</span>.</p>
+        </div>
+      </main>
+      <footer>
+        <p>© {new Date().getFullYear()} Roots</p>
+      </footer>
+    </div>
   )
 }
 
